53f11d794c98b5b141df14640325acd8b63802f1
ethanyishchan <yish.chan@gmail.com>
2016-03-23 18:13:57 -0700

    updated visualizations, added in advisories

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ from flask import Flask, request,render_template, jsonify, Response, session
 from flask.ext.cache import Cache
 import numpy as np
 
+#constants
+#lake lagunita GPS coordinates
+base_lat = 37.422570
+base_long = -122.176514
+
 # SSE "protocol" is described here: http://mzl.la/UPFyxY
 class ServerSentEvent(object):
 
@@ -41,6 +46,14 @@ class ServerSentEvent(object):
 
 app = Flask(__name__)
 subscriptions = []
+advisory_subscriptions_1 = []
+advisory_subscriptions_2 = []
+advisory_subscriptions_3 = []
+
+@app.route("/debug")
+def debug():
+    return "Currently %d subscriptions" % len(subscriptions)
+
 
 # Client code consumes like this.
 @app.route("/")
@@ -62,26 +75,120 @@ def index():
 
     return render_template('main_map.html', mymap = mymap, sndmap = sndmap)
 
-@app.route("/debug")
-def debug():
-    return "Currently %d subscriptions" % len(subscriptions)
+#receive incoming advisories from kafka server
+@app.route("/consume_advisory", methods=['POST', 'GET'])
+def consume_advisory():
+    msg = request.data
+
+    advisory_array = json.loads(msg)
+    # print advisory_array
+
+    def notify(m, adv_subs):
+        for sub in adv_subs[:]:
+            sub.put(m)
+
+    for gufi_adv in advisory_array:
+        curr_lat = float(gufi_adv['waypoints'][0]["lat"])
+        curr_lon = float(gufi_adv['waypoints'][0]["lon"])
+        new_lat, new_lon = lat_lon_convert(curr_lat,curr_lon)
+        gufi_adv['waypoints'][0]["lat"] = str(new_lat)
+        gufi_adv['waypoints'][0]["lon"] = str(new_lon)
+        gufi_msg = json.dumps(gufi_adv)
+        if gufi_adv["gufi"] == "drone1":
+            gevent.spawn(notify(  gufi_msg,    advisory_subscriptions_1))
+        elif gufi_adv["gufi"] == "drone2":
+            gevent.spawn(notify(  gufi_msg,    advisory_subscriptions_2))
+        else:
+            gevent.spawn(notify(  gufi_msg,    advisory_subscriptions_3))
+
+    # gevent.spawn(notify(msg))
+    return "OK"
+
+#drone 1 advisory view
+@app.route("/advisory_view_1")
+def advisories_view_1():
+    return render_template('drone_1_advisory.html')
+
+#drone 2 advisory view
+@app.route("/advisory_view_2")
+def advisories_view_2():
+    return render_template('drone_2_advisory.html')
+
+#drone 3 advisory view
+@app.route("/advisory_view_3")
+def advisories_view_3():
+    return render_template('drone_3_advisory.html')
+
+#drone 1 advisory "socket"
+@app.route("/advisory_drone_1")
+def advisory_1():
+    def gen():
+        q = Queue()
+        advisory_subscriptions_1.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = ServerSentEvent(str(result))
+                yield ev.encode()
+        except GeneratorExit: # Or maybe use flask signals
+            advisory_subscriptions_1.remove(q)
+    return Response(gen(), mimetype="text/event-stream")
+
+#drone 2 advisory "socket"
+@app.route("/advisory_drone_2")
+def advisory_2():
+    def gen():
+        q = Queue()
+        advisory_subscriptions_2.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = ServerSentEvent(str(result))
+                yield ev.encode()
+        except GeneratorExit: # Or maybe use flask signals
+            advisory_subscriptions_2.remove(q)
+    return Response(gen(), mimetype="text/event-stream")
+
+#drone 3 advisory "socket"
+@app.route("/advisory_drone_3")
+def advisory_3():
+    def gen():
+        q = Queue()
+        advisory_subscriptions_3.append(q)
+        try:
+            while True:
+                result = q.get()
+                ev = ServerSentEvent(str(result))
+                yield ev.encode()
+        except GeneratorExit: # Or maybe use flask signals
+            advisory_subscriptions_3.remove(q)
+    return Response(gen(), mimetype="text/event-stream")
+
+def lat_lon_convert(lat, lon):
+    lat_offset = lat / 111111.0 * 1.0
+    new_lat = base_lat + lat_offset
+    lon_offset = lon / 111111.0 * np.cos(new_lat)
+    new_lon = base_long + lon_offset
+    return new_lat, new_lon
 
 @app.route("/conflict", methods=['POST', 'GET'])
 def consume_conflict():
-    #Dummy data - pick up from request for real data
-    base_lat = 37.424106
-    base_long = -122.166076
+    # #starts at Lake Lagunita for now
+    # base_lat = 37.422570
+    # base_long = -122.176514
 
+    # 37.422570, -122.176514
     drone,lat, lon , heading= request.data.split('~')
     lat, lon , heading= float(lat), float(lon), float(heading)
     #convert it to lat meters
 
     heading = heading / np.pi * 180
 
-    lat_offset = lat / 111111.0 * 1.0
-    new_lat = base_lat + lat_offset
-    lon_offset = lon / 111111.0 * np.cos(new_lat)
-    new_lon = base_long + lon_offset
+    new_lat, new_lon = lat_lon_convert(lat,lon)
+    # lat_offset = lat / 111111.0 * 1.0
+    # new_lat = base_lat + lat_offset
+    # lon_offset = lon / 111111.0 * np.cos(new_lat)
+    # new_lon = base_long + lon_offset
     msg = drone + "~" + str(new_lat) + "~" + str(new_lon) + "~" + str(heading)
 
     # If your displacements aren't too great (less than a few kilometers) and you're not right at the poles, 
@@ -89,7 +196,6 @@ def consume_conflict():
     #and 111,111 * cos(latitude) meters in the x direction is 1 degree (of longitude).
 
     #convert it to 
-
 
     # status_update = json.loads(request.data)
     # gufi = status_update['flightId']
@@ -100,8 +206,6 @@ def consume_conflict():
     # msg =  (lat,lon)
 
     # msg = request.data
-
-
 
     def notify():
         for sub in subscriptions[:]:
